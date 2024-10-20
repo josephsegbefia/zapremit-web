28b5cf988661247e534e89dc4ec1dd9046e681cf
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-10-20 01:07:45 +0000

    Start landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className="flex justify-center items-center min-h-screen w-full border border-red-100">
-      <p className="text-bold text-lg text-teal-500">Landing Page</p>
-    </div>
+    <>
+      <div className="p-7">
+        <nav className="flex justify-between items-center fixed">
+          <Image src="/logo.svg" width={152} height={56} alt="Logo" />
+        </nav>
+      </div>
+
+      <div className="flex justify-center items-center min-h-screen w-ful">
+        <p className="text-bold text-lg text-teal-500">Landing Page</p>
+
+        <div className="flex items-center justify-center">
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={() => router.push("/sign-in")}
+          >
+            Sign In
+          </Button>
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={() => router.push("/sign-up")}
+          >
+            Sign Up
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }
