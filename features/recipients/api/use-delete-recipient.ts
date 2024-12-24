da3da7e3c2b91033c31e04670c0b9842096d7741
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-12-24 12:25:49 +0000

    Finish useDeleteRecipient hook

--- features/recipients/api/use-delete-recipient.ts
+++ features/recipients/api/use-delete-recipient.ts
@@ -2,13 +2,38 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import { toast } from "sonner";
 import { client } from "@/lib/rpc";
-import { useRouter } from "next/navigation";
 
-type RespoonseType = InferResponseType<
+type ResponseType = InferResponseType<
   (typeof client.api.recipients)[":recipientId"]["$delete"],
   200
 >;
 
 type RequestType = InferRequestType<
-  (typeof client.api.recipients)[":recipientid"]["$delete"]
+  (typeof client.api.recipients)[":recipientId"]["$delete"]
 >;
+
+export const useDeleteRecipient = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param }) => {
+      const response = await client.api.recipients[":recipientId"]["$delete"]({
+        param,
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to delete recipient");
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success("Recipient deleted");
+      queryClient.invalidateQueries({ queryKey: ["recipients"] });
+      queryClient.invalidateQueries({ queryKey: ["recipient", data.$id] });
+    },
+    onError: () => {
+      toast.error("Failed to delete recipient");
+    },
+  });
+  return mutation;
+};
